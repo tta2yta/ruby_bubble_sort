31ab7bc11a2bdb1f8cbd2920bb720c649a398614
sohaibya <sohaibasif2019@gmail.com>
2021-02-15 18:46:17 +0500

    Added if statement

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,5 +1,9 @@
 def bubblesort array
     index = 0
     while index < array.size - 1
+        if array[i] > array[i+1]
+        array[i],array[i+1] = array[i+1],array[i]
+        index =0
+        end
     end
 end

f99a8e4d5e3c7132f21b610d6e14d6574959a1a5
sohaibya <sohaibasif2019@gmail.com>
2021-02-15 20:40:33 +0500

    Bubble_sort part 1 completed

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,16 +1,16 @@
-def bubble_sort array
-    index = 0
-    while index < array.size - 1
-if  array[index] > array[index+1]
-    array[index],array[index+1] = array[index+1],array[index]
-    index =0
-   
-elseif
-    index += 1 
+#Bubble sort
+def bubble_sort(array)
+  index = 0
+  while index < array.size - 1
+    if array[index] > array[index + 1]
+      array[index], array[index + 1] = array[index + 1], array[index]
+      index = 0
+    else
+      index += 1
     end
-    end
-    puts array
-    end
+  end
+  puts array
+end
 
-array = [4,3,78,2,0,2]
+array = [4, 3, 78, 2, 0, 2]
 puts bubble_sort array
